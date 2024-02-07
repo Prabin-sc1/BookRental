e032313ff33793928bdaca725bf5466bc869e276
Prabin-sc1 <prabin.sc1@gmail.com>
2024-02-07 11:28:04 +0545

    fix: configuration added to fix cors problem

--- src/main/java/com/bookrental/bookrental/controller/AuthorController.java
+++ src/main/java/com/bookrental/bookrental/controller/AuthorController.java
@@ -27,6 +27,7 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/author")
 @Tag(name = ModuleNameConstants.AUTHOR)
+@CrossOrigin(origins = "*")
 public class AuthorController extends MyBaseController {
     private final AuthorService authorService;
 
--- src/main/java/com/bookrental/bookrental/jwt/SecurityConfig.java
+++ src/main/java/com/bookrental/bookrental/jwt/SecurityConfig.java
@@ -40,16 +40,17 @@ public class SecurityConfig {
             "/v3/api-docs"
     };
 
-//    @Bean
-//    public CorsConfigurationSource corsConfig() {
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.setAllowedOrigins(
-//                List.of("http://localhost:5173","*"));
-//        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", corsConfiguration);
-//        return source;
-//    }
+    /*@Bean
+    public CorsConfigurationSource corsConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(
+                List.of("http://localhost:5173","*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        corsConfiguration.addAllowedHeader();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }*/
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
